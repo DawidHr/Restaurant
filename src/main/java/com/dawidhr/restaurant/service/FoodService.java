32f11bc3b167e469cfb320c7f507bb715be9cd6f
DawidHr <dawiid21@gmail.com>
2025-10-15 20:51:29 +0200

    add Food Controller and Service

--- src/main/java/com/dawidhr/restaurant/service/FoodService.java
+++ src/main/java/com/dawidhr/restaurant/service/FoodService.java
@@ -1,15 +1,22 @@
 package com.dawidhr.restaurant.service;
 
-import com.dawidhr.restaurant.repository.FoodRepository;
+import com.dawidhr.restaurant.dao.FoodDao;
+import com.dawidhr.restaurant.dto.FoodDto;
+import com.dawidhr.restaurant.model.Food;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FoodService {
 
     @Autowired
-    FoodRepository foodRepository;
+    FoodDao foodDao;
 
-    public void getList() {
+    public List<FoodDto> getList() {
+        List<Food> food = foodDao.getFoodList();
+        List<FoodDto> results = food.stream().map(FoodDto::create).toList();
+        return results;
     }
 }
