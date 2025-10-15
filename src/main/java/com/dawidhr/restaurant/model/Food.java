2612fa324adbc05744b944262a848b0d60f8f01c
DawidHr <dawiid21@gmail.com>
2025-10-15 19:59:16 +0200

    add Food entity

---
+++ src/main/java/com/dawidhr/restaurant/model/Food.java
@@ -0,0 +1,98 @@
+package com.dawidhr.restaurant.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
+
+    @Column(length = Integer.MAX_VALUE)
+    private String description;
+
+    @Column(name = "img_path")
+    private String imgPath;
+
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+    private Double price;
+
+    public Food() {
+    }
+
+    public Food(Long id, String name, String description, String imgPath, LocalDateTime creationDate, LocalDateTime updateDate, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imgPath = imgPath;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgPath() {
+        return imgPath;
+    }
+
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
