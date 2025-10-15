57fc45317b75f401943431f9ca13772bd5551af8
DawidHr <dawiid21@gmail.com>
2025-10-15 20:11:49 +0200

    add Order and OrderItem

---
+++ src/main/java/com/dawidhr/restaurant/model/Order.java
@@ -0,0 +1,96 @@
+package com.dawidhr.restaurant.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @UpdateTimestamp
+    @Column(name="update_date")
+    private LocalDateTime updateDate;
+    private String status;
+    private long tableId;
+    private double totalPrice;
+    private List<OrderItem> items;
+
+    public Order() {
+    }
+
+    public Order(Long id, LocalDateTime creationDate, LocalDateTime updateDate, String status, long tableId, double totalPrice, List<OrderItem> items) {
+        this.id = id;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+        this.status = status;
+        this.tableId = tableId;
+        this.totalPrice = totalPrice;
+        this.items = items;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getTableId() {
+        return tableId;
+    }
+
+    public void setTableId(long tableId) {
+        this.tableId = tableId;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/com/dawidhr/restaurant/model/OrderItem.java
@@ -0,0 +1,37 @@
+package com.dawidhr.restaurant.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, Food food) {
+        this.id = id;
+        this.food = food;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public void setFood(Food food) {
+        this.food = food;
+    }
+}
