2370bac12f41310a291496a9e65a49c9f731c221
DawidHr <dawiid21@gmail.com>
2025-10-16 20:09:44 +0200

    Add lombok to FoodDto

--- src/main/java/com/dawidhr/restaurant/dto/FoodDto.java
+++ src/main/java/com/dawidhr/restaurant/dto/FoodDto.java
@@ -1,7 +1,13 @@
 package com.dawidhr.restaurant.dto;
 
 import com.dawidhr.restaurant.model.Food;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class FoodDto {
     private long id;
     private String name;
@@ -9,64 +15,13 @@ public class FoodDto {
     private String imgPath;
     private Double price;
 
-    public FoodDto() {
-    }
-
-    public FoodDto(long id, String name, String description, String imgPath, Double price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.imgPath = imgPath;
-        this.price = price;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImgPath() {
-        return imgPath;
-    }
-
-    public void setImgPath(String imgPath) {
-        this.imgPath = imgPath;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
     public static FoodDto create(Food food) {
-        FoodDto foodDto = new FoodDto();
-        foodDto.setId(food.getId());
-        foodDto.setName(food.getName());
-        foodDto.setDescription(food.getDescription());
-        foodDto.setPrice(food.getPrice());
-        foodDto.setImgPath(food.getImgPath());
-        return foodDto;
+        return FoodDto.builder()
+                .id(food.getId())
+                .name(food.getName())
+                .description(food.getDescription())
+                .price(food.getPrice())
+                .imgPath(food.getImgPath())
+                .build();
     }
 }
