9ff5ccdd6dd8d40b38b20ab4347939ac7b700fac
DawidHr <dawiid21@gmail.com>
2025-10-16 19:40:41 +0200

    add Order Service

---
+++ src/main/java/com/dawidhr/restaurant/service/OrderService.java
@@ -0,0 +1,14 @@
+package com.dawidhr.restaurant.service;
+
+
+import com.dawidhr.restaurant.dto.OrderDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+
+    public void createOrder(OrderDto orderDto) {
+
+    }
+}
