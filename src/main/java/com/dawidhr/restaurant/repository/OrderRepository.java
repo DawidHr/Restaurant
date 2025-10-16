de6c4e149c31147920efd5da8f991edb99ba5247
DawidHr <dawiid21@gmail.com>
2025-10-16 19:53:45 +0200

    add Order Repository

---
+++ src/main/java/com/dawidhr/restaurant/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.dawidhr.restaurant.repository;
+
+import com.dawidhr.restaurant.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
