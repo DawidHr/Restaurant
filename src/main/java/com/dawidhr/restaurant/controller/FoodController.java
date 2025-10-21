b0f3b47ce11e00018930f0405bbe7c480c1c3ebb
DawidHr <dawiid21@gmail.com>
2025-10-21 21:29:52 +0200

    update food endpoint

--- src/main/java/com/dawidhr/restaurant/controller/FoodController.java
+++ src/main/java/com/dawidhr/restaurant/controller/FoodController.java
@@ -1,10 +1,14 @@
 package com.dawidhr.restaurant.controller;
 
+import com.dawidhr.restaurant.dto.FoodDto;
 import com.dawidhr.restaurant.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/food")
@@ -13,7 +17,8 @@ public class FoodController {
     FoodService foodService;
 
     @GetMapping
-    public void getList() {
-        foodService.getList();
+    public ResponseEntity<List<FoodDto>> getList() {
+        List<FoodDto> response = foodService.getList();
+        return ResponseEntity.ok(response);
     }
 }
